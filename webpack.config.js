b85bc26aa666b1a987fea0e595e85df74a9afb65
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2018-07-13 22:52:42 +0900

    Enable webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    'index': path.resolve(__dirname, 'src', 'index.js')
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  }
+};
