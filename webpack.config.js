e0c1b8db2b0a75bb33303302253dd06eb9d6829f
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2018-07-13 23:11:20 +0900

    Enable webpack without packing code in eval

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {
     'index': path.resolve(__dirname, 'src', 'index.js')
   },
 
+  devtool: false,
+
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
