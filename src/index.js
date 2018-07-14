d6a8ad6fe75cc371d6841c652530954c8302ff87
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2018-07-15 04:09:58 +0900

    Reorganize as a class

--- src/index.js
+++ src/index.js
@@ -5,72 +5,86 @@ import 'leaflet.vectorgrid';
 
 import './index.css';
 
-import euCountriesJson from './eu-countries.json';
-var request = new XMLHttpRequest();
-request.open("GET", euCountriesJson, false);
-request.send(null);
-var euCountries = JSON.parse(request.responseText);
+class VectorMap {
+  constructor (mapId) {
+    this.map = L.map(mapId);
+    const tileURL = 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png';
+    const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+    this.setupTileLayer(tileURL, attribution);
 
-var map = L.map('map');
+    const jsonFile = require('./eu-countries.json');
+    const request = new XMLHttpRequest();
+    request.open("GET", jsonFile, false);
+    request.send(null);
+    const polygon = JSON.parse(request.responseText);
+    this.setupVectorGridLayer(polygon);
 
-var cartodbAttribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+    this.map.on('click', () => this.setHighlight());
+    this.map.setView({lat: 47.040182144806664, lng: 9.667968750000002}, 5);
+  }
 
-var positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-  attribution: cartodbAttribution,
-  opacity: 1
-}).addTo(map);
+  setupTileLayer(tileURL, attribution) {
+    this.tileLayer = L.tileLayer(tileURL, {
+      attribution: attribution,
+      opacity: 1
+    });
+    this.tileLayer.addTo(this.map);
+  }
 
-var highlight;
-var polygonStyle = function(mapcolor7, forHighlight) {
-  var p = mapcolor7 % 5;
-  return {
-    fillColor: p === 0 ? '#800026' :
-      p === 1 ? '#E31A1C' :
-      p === 2 ? '#FEB24C' :
-      p === 3 ? '#B2FE4C' : '#FFEDA0',
-    fillOpacity: forHighlight ? 1 : 0.5,
-    stroke: true,
-    fill: true,
-    color: forHighlight ? 'red' : 'black',
-    opacity: 1,
-    weight: forHighlight ? 2 : 0
+  setupVectorGridLayer(polygon) {
+    this.vectorGridLayer = L.vectorGrid.slicer(polygon, {
+      rendererFactory: L.svg.tile,
+      vectorTileLayerStyles: {
+        sliced: (properties, zoom) => this.fillPolygon(properties, zoom)
+      },
+      interactive: true,
+      getFeatureId: function(f) {
+        return f.properties.wb_a3;
+      }
+    });
+    this.vectorGridLayer.on('mouseover', (e) => this.highlightPolygon(e));
+    this.vectorGridLayer.addTo(this.map);
+  }
+
+  polygonStyle(mapcolor7, forHighlight) {
+    const p = mapcolor7 % 5;
+    return {
+      fillColor: p === 0 ? '#800026' :
+        p === 1 ? '#E31A1C' :
+        p === 2 ? '#FEB24C' :
+        p === 3 ? '#B2FE4C' : '#FFEDA0',
+      fillOpacity: forHighlight ? 1 : 0.5,
+      stroke: true,
+      fill: true,
+      color: forHighlight ? 'red' : 'black',
+      opacity: 1,
+      weight: forHighlight ? 2 : 0
+    };
+  }
+
+  setHighlight(polygon) {
+    if (this.setHighlight.highlightedPolygon) {
+      this.vectorGridLayer.resetFeatureStyle(this.setHighlight.highlightedPolygon);
+    }
+    this.setHighlight.highlightedPolygon = polygon;
+  }
+
+  highlightPolygon(e) {
+    const properties = e.layer.properties;
+    L.popup()
+      .setContent(properties.name || properties.type)
+      .setLatLng(e.latlng)
+      .openOn(this.map);
+
+    this.setHighlight(properties.wb_a3);
+
+    const style = this.polygonStyle(properties.mapcolor7, true);
+    this.vectorGridLayer.setFeatureStyle(properties.wb_a3, style);
+  }
+
+  fillPolygon(properties, zoom) {
+    return this.polygonStyle(properties.mapcolor7, false);
   };
-};
-var clearHighlight = function() {
-  if (highlight) {
-    vectorGrid.resetFeatureStyle(highlight);
-  }
-  highlight = null;
-};
-var highlightPolygon = function(e) {
-  var properties = e.layer.properties;
-  L.popup()
-    .setContent(properties.name || properties.type)
-    .setLatLng(e.latlng)
-    .openOn(map);
+}
 
-  clearHighlight();
-  highlight = properties.wb_a3;
-
-  var style = polygonStyle(properties.mapcolor7, true);
-  vectorGrid.setFeatureStyle(properties.wb_a3, style);
-};
-var fillPolygon = function(properties, zoom) {
-  return polygonStyle(properties.mapcolor7, false);
-};
-var vectorGrid = L.vectorGrid.slicer( euCountries, {
-  rendererFactory: L.svg.tile,
-  vectorTileLayerStyles: {
-    sliced: fillPolygon
-  },
-  interactive: true,
-  getFeatureId: function(f) {
-    return f.properties.wb_a3;
-  }
-})
-    .on('mouseover', highlightPolygon)
-    .addTo(map);
-
-map.on('click', clearHighlight);
-
-map.setView({ lat: 47.040182144806664, lng: 9.667968750000002 }, 5);
+var vm = new VectorMap('map');
